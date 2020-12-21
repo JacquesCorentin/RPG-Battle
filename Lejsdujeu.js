781887249504871b6a408594b9e60ab4e85a9324
JacquesCorentin <74654858+JacquesCorentin@users.noreply.github.com>
2020-12-21 21:21:04 +0100

    Update Lejsdujeu.js

--- Lejsdujeu.js
+++ Lejsdujeu.js
@@ -1,20 +1,3 @@
-document.getElementsById('Monstre1').onmouseover = function(){
-  document.getElementsById("Stats1").style.visibility = "visible";
-}
-document.getElementsById('Monstre1').onmouseover = function(){
-  document.getElementsById("Stats1").style.visibility = "hidden";
-}
-
-document.getElementsById('Monstre2').onmouseover = function(){
-  document.getElementsById("Stats2").style.visibility = "visible";
-}
-document.getElementsById('Monstre2').onmouseover = function(){
-  document.getElementsById("Stats2").style.visibility = "hidden";
-}
-
-document.getElementsById('Monstre3').onmouseover = function(){
-  document.getElementsById("Stats3").style.visibility = "visible";
-}
-document.getElementsById('Monstre3').onmouseover = function(){
-  document.getElementsById("Stats3").style.visibility = "hidden";
-}
+var monstre1 = document.getElementById('monstre1')
+var monstre1 = document.getElementById('monstre2')
+var monstre1 = document.getElementById('monstre3')
