93e17a0d59167a7cbbe521d8feb9c5aeb7b48efa
JacquesCorentin <74654858+JacquesCorentin@users.noreply.github.com>
2020-12-22 00:36:24 +0100

    23

--- Lejsdujeu.js
+++ Lejsdujeu.js
@@ -1,11 +1,16 @@
-var monstre1 = document.getElementById('monstre1')
-var monstre1 = document.getElementById('monstre2')
-var monstre1 = document.getElementById('monstre3')
-
+// Variables //
+var monstre1 = document.getElementById('Monstre1')
+var monstre2 = document.getElementById('Monstre2')
+var monstre3 = document.getElementById('Monstre3')
+//**Actions**//
 var attaquer = document.getElementById("BoutonAttaquer")
 var defense = document.getElementById("BoutonDefense")
 var poison = document.getElementById("BoutonPoison")
 var soin = document.getElementById("BoutonSoin")
+
+var choixSlime = document.getElementById("btnSlime")
+var choixSlume = document.getElementById("btnSlume")
+var choixFlime = document.getElementById("btnFlime")
 
 var Justin = document.getElementById("boutonHugo")
 var Kévin = document.getElementById("boutonTom")
@@ -30,3 +35,31 @@ var choixMonstres = 0
 var monstre = 0
 var tour = 0
 
+// Les infobulles //
+
+monstre1.onmouseover = function(){
+  document.getElementById("boxMonstre1").style.visibility = "visible" ;
+}
+monstre1.onmouseout = function(){
+  document.getElementById("boxMonstre1").style.visibility = "hidden" ;
+}
+    
+monstre2.onmouseover = function(){
+  document.getElementById("boxMonstre2").style.visibility = "visible" ;
+}
+monstre2.onmouseout = function(){
+  document.getElementById("boxMonstre2").style.visibility = "hidden" ;
+}
+    
+monstre3.onmouseover = function(){
+  document.getElementById("boxMonstre3").style.visibility = "visible" ;
+}
+monstre3.onmouseout = function(){
+  document.getElementById("boxMonstre3").style.visibility = "hidden" ;
+}    
+//
+//choixSlime.onclick = function(){
+  //document.getElementById("boxMonstre1").style.visibility = "visible" 
+  //document.getElementById("boxMonstre2").style.visibility = "hidden"
+  //document.getElementById("boxMonstre3").style.visibility = "hidden"
+  //choixJoueurs = 1 //
