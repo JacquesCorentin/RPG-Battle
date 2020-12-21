0b6bae3d57f18fa132330008673ef2100e471325
JacquesCorentin <74654858+JacquesCorentin@users.noreply.github.com>
2020-12-21 21:29:30 +0100

    Update Lejsdujeu.js

--- Lejsdujeu.js
+++ Lejsdujeu.js
@@ -1,3 +1,31 @@
 var monstre1 = document.getElementById('monstre1')
 var monstre1 = document.getElementById('monstre2')
 var monstre1 = document.getElementById('monstre3')
+
+var attaquer = document.getElementById("BoutonAttaquer")
+var defense = document.getElementById("BoutonDefense")
+var poison = document.getElementById("BoutonPoison")
+var soin = document.getElementById("BoutonSoin")
+
+var Justin = document.getElementById("boutonHugo")
+var Kévin = document.getElementById("boutonTom")
+var Ventux = document.getElementById("boutonAntoine") 
+
+var manaJustin = 100
+var manaKévin = 100
+var manaVentux = 100
+
+var attaqueJoueur = [10, 25, 5, 2 ,25] 
+var attaqueMonstre = [5, 10, 15, 20 ,25]
+
+var hpMonstre = [0, 200, 150, 250]
+var hpJoueur = [0, 150, 150, 150, 150]
+
+var dmgPoison = 5
+var heal = 20
+
+var choixJoueurs = 0
+var choixMonstres = 0
+
+var monstre = 0
+var tour = 0
