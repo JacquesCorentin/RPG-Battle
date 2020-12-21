c799ce8a267cf4cd4b0a0308635f4e0b66fc8e88
JacquesCorentin <74654858+JacquesCorentin@users.noreply.github.com>
2020-12-21 18:44:57 +0100

    n,

--- Lejsdujeu.js
+++ Lejsdujeu.js
@@ -1,16 +1,20 @@
-let VieDuMonstre1 = document.getElementById("VieDuMonstre1")
-VieDuMonstre1.valeur -= 10;
+document.getElementsById('Monstre1').onmouseover = function(){
+  document.getElementsById("Stats1").style.visibility = "visible";
+}
+document.getElementsById('Monstre1').onmouseover = function(){
+  document.getElementsById("Stats1").style.visibility = "hidden";
+}
 
-function timedProg() {
-  if (i <= 300) {
-    if (i > 40) {
-            document.getElementById("c").innerHTML=parseInt(i/3)+"%";
-            }
-    document.getElementById("b").style.width=i+"px";
-    var j=0;       
-    while (j<=100)
-         j++; 
-    setTimeout("timedProg();", 20);
-    i++;   
-    }
-  }
+document.getElementsById('Monstre2').onmouseover = function(){
+  document.getElementsById("Stats2").style.visibility = "visible";
+}
+document.getElementsById('Monstre2').onmouseover = function(){
+  document.getElementsById("Stats2").style.visibility = "hidden";
+}
+
+document.getElementsById('Monstre3').onmouseover = function(){
+  document.getElementsById("Stats3").style.visibility = "visible";
+}
+document.getElementsById('Monstre3').onmouseover = function(){
+  document.getElementsById("Stats3").style.visibility = "hidden";
+}
